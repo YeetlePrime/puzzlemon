68c90cc21f0e3fb5c2e435148f10a61d9cc0542d
YeetlePrime <jonas.tolkemitt@gmail.com>
2025-01-07 22:11:08 +0100

    Add application and guild id to environment

--- app/config.js
+++ app/config.js
@@ -1,4 +1,6 @@
 export const token = process.env.TOKEN;
+export const applicationId = process.env.APPLICATION_ID;
+export const guildId = process.env.GUILD_ID;
 export const dbConfig = {
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
