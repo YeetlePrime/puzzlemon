bcf0ba79655f3dc69f95eeae06fd30387291ebf3
YeetlePrime <jonas.tolkemitt@gmail.com>
2025-02-05 14:15:08 +0100

    Add build step to quadlet script

--- quadlet.sh
+++ quadlet.sh
@@ -5,7 +5,9 @@ script_dir=$(dirname "${script}")
 env_file="${script_dir}/.env"
 
 sudo mkdir -p /etc/containers/systemd/
-sed "s|@ENV_FILE@|${env_file}|g" ./quadlets/puzzlemon-db.volume | sudo tee /etc/containers/systemd/puzzlemon-db.volume
-sed "s|@ENV_FILE@|${env_file}|g" ./quadlets/puzzlemon.network | sudo tee /etc/containers/systemd/puzzlemon.network
-sed "s|@ENV_FILE@|${env_file}|g" ./quadlets/puzzlemon-db.container | sudo tee /etc/containers/systemd/puzzlemon-db.container
-sed "s|@ENV_FILE@|${env_file}|g" ./quadlets/puzzlemon.container | sudo tee /etc/containers/systemd/puzzlemon.container
+sed "s|@ENV_FILE@|${env_file}|g" ./quadlets/puzzlemon-db.volume | sudo tee /etc/containers/systemd/puzzlemon-db.volume >/dev/null
+sed "s|@ENV_FILE@|${env_file}|g" ./quadlets/puzzlemon.network | sudo tee /etc/containers/systemd/puzzlemon.network >dev/null
+sed "s|@ENV_FILE@|${env_file}|g" ./quadlets/puzzlemon-db.container | sudo tee /etc/containers/systemd/puzzlemon-db.container >dev/null
+sed "s|@ENV_FILE@|${env_file}|g" ./quadlets/puzzlemon.container | sudo tee /etc/containers/systemd/puzzlemon.container >dev/null
+
+sudo podman build -t puzzlemon-app .
