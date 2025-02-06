ed9ce589dc46a226b627c490838735a574d0f0dc
YeetlePrime <jonas.tolkemitt@gmail.com>
2025-02-06 20:25:19 +0100

    Add additional logging to a directory

--- deploy_systemd.sh
+++ deploy_systemd.sh
@@ -4,6 +4,7 @@ script=$(realpath -s "$0")
 script_dir=$(dirname "${script}")
 env_file="${script_dir}/.env"
 container_file="${script_dir}/Containerfile"
+podman_log_dir="${HOME}/podman_logs"
 
 # set target directory for the quadlet files
 if [ -z "${XDG_CONFIG_HOME}" ]; then
@@ -19,12 +20,16 @@ replace_and_copy() {
 
 	sed -e "s|@ENV_FILE@|${env_file}|g" \
 		-e "s|@CONTAINER_FILE@|${container_file}|g" \
+		-e "s|@PODMAN_LOG_DIR@|${podman_log_dir}|g" \
 		"${file}" >"${file_target}"
 
 	echo "${file} --> ${file_target}"
 }
 
 mkdir -p "${target_dir}"
+mkdir -p "${podman_log_dir}"
+chmod 666 "${podman_log_dir}"
+
 for file in "${script_dir}/quadlets/"*; do
 	replace_and_copy "${file}"
 done
